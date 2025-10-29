bd74445a4ddc0deb6d25985fb2bd3306979d30ed
MudassirQureshi7987 <mudassirmohammad7987@gmail.com>
2025-10-29 19:19:31 +0530

    LeetMetric Project in the making

--- ProjectOne/Lecture 59 Project LeetMetrics/script.js
+++ ProjectOne/Lecture 59 Project LeetMetrics/script.js
@@ -11,4 +11,28 @@ document.addEventListener("DOMContentLoaded", function() {
     const mediumLabel = document.getElementById("medium-label");
     const hardLabel = document.getElementById("hard-label");
     const cardStatsContainer = document.querySelector(".stats-cards");
+
+    function validateUsername(username) {
+        if(username.trim() === "") {
+            alert("Please enter a valid LeetCode username.");
+            return false;
+        }
+
+        const regex = /^[a-zA-Z0-9_-]+$/;
+        const isValid = regex.test(username);
+        if(!isValid) {
+            alert("Username can only contain letters, numbers, underscores, and hyphens.");
+        }
+        return isValid;
+    }
+
+    async function fetchLeetCodeData(username) {
+        if(!validateUsername(username)) {
+            return;
+        }
+        const url = 'https://leetcode-stats-api.herokuapp.com/${username}}';
+    searchButton.addEventListener("click", function() {
+        const username = usernameInput.value.trim();
+        console.log("Searching for user:", username);
+    });
 });
