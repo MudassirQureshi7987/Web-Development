1c395b9547b637cde010ae8d2b983e284e2305eb
MudassirQureshi7987 <mudassirmohammad7987@gmail.com>
2026-08-01 18:45:24 +0530

    Changes

--- Javascript MasterClass/Phase 2/LocalStorage,SessionStorage and Cookies/script.js
+++ Javascript MasterClass/Phase 2/LocalStorage,SessionStorage and Cookies/script.js
@@ -156,4 +156,37 @@
 
 // Now we will make a website which changes its mode according to the systems mode dark or light
 
+function setDarkorLight() {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    } else {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+}
 
+setDarkorLight(); // will run initially when the website reloads or opens
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click",function() {
+    if(document.body.classList.contains("dark")) {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+
+        // without this,if we close the tab then the screen stays dark and does not change so
+        localStorage.setItem("theme","light");
+    } else {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+
+        localStorage.setItem("theme","dark");
+    }
+});
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change",function() {
+    setDarkorLight();
+});
+
+// This Event Listener will run whenever we change the theme of our system
---
+++ Javascript MasterClass/Phase 3/Scope,Execution Context and Closures/script.js
@@ -0,0 +1,5 @@
+function abcd() {
+    var a = 30;
+}
+
+console.log(a);
