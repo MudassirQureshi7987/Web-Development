e95de928ede1cc6fe7585421c0dba91f086024c4
MudassirQureshi7987 <mudassirmohammad7987@gmail.com>
2026-08-31 11:12:53 +0530

    Commit

--- BackEnd/04-REST-API-task/src/app.js
+++ BackEnd/04-REST-API-task/src/app.js
@@ -35,4 +35,17 @@ app.delete('/notes/:index',(req,res) => {
         message : "Note deleted successfully"
     })
 })
+
+app.patch("/notes/:index", (req,res) => {
+    const index = req.params.index;
+    const description = req.body.description;
+
+    notes[ index ].description = description;
+
+    res.status(200).json({
+        message : "Note updated successfully"   
+    })
+})
+
+
 module.exports = app
