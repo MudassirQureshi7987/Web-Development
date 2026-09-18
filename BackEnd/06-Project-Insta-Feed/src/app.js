b302eb954187b246e0df012287882eb5d4a51e1b
MudassirQureshi7987 <mudassirmohammad7987@gmail.com>
2026-09-18 14:33:38 +0530

    Commit

--- BackEnd/06-Project-Insta-Feed/src/app.js
+++ BackEnd/06-Project-Insta-Feed/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const multer = require("multer")
 const uploadFile = require("./services/storage.service")
+const postModel = require("./models/post.model")
 
 const app = express()
 app.use(express.json())
@@ -13,12 +14,24 @@ app.post("/create-post", upload.single("image"), async (req, res) => {
 
     const result = await uploadFile(req.file.buffer)
 
-    console.log(result)
+    const post = await postModel.create({
+        image : result.url,
+        caption : req.body.caption
+    })
 
-    res.status(200).json({
-        message : "Post created successfully"
+    res.status(201).json({
+        message : "Post created successfully",
+        post
     })
 })
 
+app.get("/posts", async (req, res) => {
+    const posts = await postModel.find()
+
+    res.status(200).json({
+        message : "Posts fetced successfully",
+        posts
+    })
+})
 
 module.exports = app
